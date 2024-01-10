2e892d7bf019a37a5f1f45f99e2123523ba0b7bb
duoquote <16341937+Duoquote@users.noreply.github.com>
2024-01-10 13:30:12 +0300

    Update dependencies and fix import paths

--- scripts/deno-build.js
+++ scripts/deno-build.js
@@ -2,6 +2,3 @@ import { bundle } from "https://deno.land/x/emit/mod.ts";
 
 var { code } = await bundle("./src/index.ts");
 Deno.writeTextFileSync("bin/jsencrypt.bundle.js", code);
-
-var { code } = await bundle("./src/JSEncryptRSAKey.ts");
-Deno.writeTextFileSync("bin/jsencryptRSA.bundle.js", code);
